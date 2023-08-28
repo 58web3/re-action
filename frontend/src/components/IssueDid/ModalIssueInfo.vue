e26f22a51158a786c5bd0db0f2c1b3f4e8eb1a5c
ext-linhchau <ext-linhchau@spdigital.sg>
2023-08-28 11:11:21 +0700

    Fix responsive for conent issue ID and issue VC

--- frontend/src/components/IssueDid/ModalIssueInfo.vue
+++ frontend/src/components/IssueDid/ModalIssueInfo.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="wrap-info">
     <p>Are those information correct ?</p>
-    <div class="d-flex flex-column">
+    <div class="d-flex flex-column wrap-content-item">
       <div class="row">
-        <div class="col-lg-6 d-flex justify-content-end">First Name</div>
-        <div class="col-lg-6 d-flex justify-content-start">{{ firstName }}</div>
+        <div
+          class="col-lg-5 col-md-6 col-5 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          First Name
+        </div>
+        <div
+          class="col-lg-7 col-md-6 col-7 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          <p class="item-content">{{ firstName }}</p>
+        </div>
       </div>
       <div class="row">
-        <div class="col-lg-6 d-flex justify-content-end">Last Name</div>
-        <div class="col-lg-6 d-flex justify-content-start">{{ lastName }}</div>
+        <div
+          class="col-lg-5 col-md-6 col-5 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          Last Name
+        </div>
+        <div
+          class="col-lg-7 col-md-6 col-7 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          <p class="item-content">{{ lastName }}</p>
+        </div>
       </div>
       <div class="row">
-        <div class="col-lg-6 d-flex justify-content-end">Email</div>
-        <div class="col-lg-6 d-flex justify-content-start">
-          {{ email }}
+        <div
+          class="col-lg-5 col-md-6 col-5 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          Email
+        </div>
+        <div
+          class="col-lg-7 col-md-6 col-7 d-flex justify-content-lg-start justify-content-md-start"
+        >
+          <p class="item-content">{{ email }}</p>
         </div>
       </div>
     </div>
@@ -48,5 +70,20 @@ export default {
 <style>
 .wrap-info {
   width: 420px;
+  @media all and (max-width: 750px) {
+    width: auto !important;
+  }
+}
+.wrap-content-item {
+  width: 65%;
+  margin: 0 auto;
+  @media all and (max-width: 750px) {
+    width: 100%;
+  }
+}
+.item-content {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
